6b1b104d77025485292334b7158bb0c3aa374ea7
qwertycorvo <mikelra54@gmail.com>
2026-05-23 20:22:17 +0800

    fix: remove unused import and ignore build warnings on CI

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Navbar from './Navbar';
 import Footer from './Footer';
